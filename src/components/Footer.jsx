4891f2832fd073427efe38c9ce37f93ba19e8635
Priyanshu Arya <91243140+priyanshuarya1213@users.noreply.github.com>
2023-11-04 14:54:36 +0530

    complete project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p style={{color: 'black'}}>Copyright ⓒ {year}</p>
     </footer>
   );
 }
